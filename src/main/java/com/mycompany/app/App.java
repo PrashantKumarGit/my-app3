5d53aa5d2f1055a49508d830a34e00a0f683db45
PrashantKumarGit <85401464+PrashantKumarGit@users.noreply.github.com>
2021-06-07 20:43:41 +0530

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello Prashant Kumar!" );
+        System.out.println( "Hello Prashant Kumar! How are you?" );
     }
 }
